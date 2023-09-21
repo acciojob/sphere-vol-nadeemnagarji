9e92e02eb079dd2f56a6751b45dcb604f2843bec
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-09-21 13:35:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function volume_sphere() {
-    //Write your code here
-  
+ const MyForm = document.querySelector("#MyForm")
+	const volume = document.querySelector("#volume")
+function volume_sphere(e) {
+	e.preventDefault()
+  const radius = document.querySelector("#radius").value
+
+		const num = Number(radius)
+		const ans = (4/3) * Math.PI * (num * num * num)
+		    volume.value =`${ans}`
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+MyForm.addEventListener('submit', volume_sphere);
